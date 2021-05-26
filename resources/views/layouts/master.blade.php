669c40a9d05a284463d4a9d7a28f27029033a668
Ayoubboujanna <ayoubbj4@gmail.com>
2021-05-26 16:10:49 +0100

    userlast modifications

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -100,6 +100,7 @@
 
                                             <div class="dropdown-menu dropdown-menu-right"
                                                 aria-labelledby="navbarDropdown">
+                                                <a class="dropdown-item" href="{{ route('checkout.show') }}">Mes commandes</a>
                                                 <a class="dropdown-item" href="{{ route('basket.show') }}">Panier</a>
                                                 <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                     document.getElementById('logout-form').submit();">
